e97f1c81013b3fc7b946689fd8c71f81753a04ce
Sandali Hasara Perera <56555623+SandaliPerera@users.noreply.github.com>
2020-11-22 23:41:38 +0530

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,12 @@
+//index number 18020569
+
+package calculator;
+
+public class SimpleCalculator{
+	
+	public double intrestCalculation(double payment,double rate){
+		double interestAmount = payment * rate /100;
+		return interestAmount;
+	}
+}
+
