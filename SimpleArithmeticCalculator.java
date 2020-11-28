cc3f6b9ca2263406126e9a7b0444d624fdf07fda
Sandali Hasara Perera <56555623+SandaliPerera@users.noreply.github.com>
2020-11-28 23:42:11 +0530

    Add files via upload

---
+++ SimpleArithmeticCalculator.java
@@ -0,0 +1,22 @@
+//index number 18020569
+
+package arithmeticCalculator;
+
+public class SimpleArithmeticCalculator{
+		
+	public double add(double n1,double n2){
+		return n1+n2;	
+	}
+	public double subtract(double n1,double n2){
+		return n1-n2;	
+	}
+	public double division(double n1,double n2){
+		return n1/n2;	
+	}
+	public double multiplication(double n1,double n2){
+		return n1*n2;	
+	}
+	
+}
+
+
